181ef35b1d4f63e5d97070baa743867a958fdeb7
Pro <pranavvyas4399@gmail.com>
2018-09-01 19:29:45 +0530

    Added ProgressBar Added support for Real device

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,37 +1,45 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.vyas.pranav.androidjokelib.JokeTellingActivityLib;
+import com.vyas.pranav.javajokelibrary.Joker;
 
-public class MainActivity extends AppCompatActivity {
+import static com.vyas.pranav.androidjokelib.JokeTellingActivityLib.JOKE_KEY;
+
+
+public class MainActivity extends AppCompatActivity implements EndpointsAsyncTask.AsyncCallback{
+    private static final String TAG = "MainActivity";
+    ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progress = new ProgressBar(this);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -40,8 +48,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker jokerJavaLib = new Joker();
+        String joke = jokerJavaLib.getJoke();
+        Log.d(TAG, "tellJoke: Starting ProgressBar");
+        progress.setVisibility(View.VISIBLE);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, joke));
     }
 
 
+    @Override
+    public void getString(String jokeString) {
+        Toast.makeText(this, jokeString, Toast.LENGTH_LONG).show();
+        progress.setVisibility(View.GONE);
+        Log.d(TAG, "getString: Stopping ProgressBar");
+        Intent intent = new Intent(this, JokeTellingActivityLib.class);
+        intent.putExtra(JOKE_KEY,jokeString);
+        startActivity(intent);
+    }
 }
