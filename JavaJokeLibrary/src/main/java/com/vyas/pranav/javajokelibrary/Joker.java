c7996144d3ba7bdb0e37a4c03b351eff5fc35f9b
Pro <pranavvyas4399@gmail.com>
2018-09-01 19:19:55 +0530

    Add Java Library

---
+++ JavaJokeLibrary/src/main/java/com/vyas/pranav/javajokelibrary/Joker.java
@@ -0,0 +1,11 @@
+package com.vyas.pranav.javajokelibrary;
+
+public class Joker {
+
+    String Joke;
+
+    public String getJoke(){
+        Joke = "You Wanted Joke !?? Ha Ha HA HA AHHAHAHHAAHAH";
+        return Joke;
+    }
+}
